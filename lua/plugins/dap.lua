07a824aec6e7f049aef59de3bfdba7d005887ab4
andri_purwanto_x <andripurwanto540@gmail.com>
2024-10-26 10:39:26 +0700

    Adjustment dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,9 +2,156 @@ return {
   'mfussenegger/nvim-dap',
   dependencies = {
     'rcarriga/nvim-dap-ui',
-    'theHamsta/nvim-dap-virtual-text',
-    -- 'nvim-telescope/telescope-dap.nvim',
+    dependencies = { 'nvim-neotest/nvim-nio' },
+    {
+      'theHamsta/nvim-dap-virtual-text',
+      opts = {},
+    },
   },
+
+  keys = {
+    {
+      '<leader>d',
+      '',
+      desc = '+debug',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>dB',
+      function()
+        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end,
+      desc = 'Breakpoint Condition',
+    },
+    {
+      '<leader>db',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'Toggle Breakpoint',
+    },
+    {
+      '<leader>dc',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Continue',
+    },
+    {
+      '<leader>da',
+      function()
+        require('dap').continue { before = get_args }
+      end,
+      desc = 'Run with Args',
+    },
+    {
+      '<leader>dC',
+      function()
+        require('dap').run_to_cursor()
+      end,
+      desc = 'Run to Cursor',
+    },
+    {
+      '<leader>dg',
+      function()
+        require('dap').goto_()
+      end,
+      desc = 'Go to Line (No Execute)',
+    },
+    {
+      '<leader>di',
+      function()
+        require('dap').step_into()
+      end,
+      desc = 'Step Into',
+    },
+    {
+      '<leader>dj',
+      function()
+        require('dap').down()
+      end,
+      desc = 'Down',
+    },
+    {
+      '<leader>dk',
+      function()
+        require('dap').up()
+      end,
+      desc = 'Up',
+    },
+    {
+      '<leader>dl',
+      function()
+        require('dap').run_last()
+      end,
+      desc = 'Run Last',
+    },
+    {
+      '<leader>do',
+      function()
+        require('dap').step_out()
+      end,
+      desc = 'Step Out',
+    },
+    {
+      '<leader>dO',
+      function()
+        require('dap').step_over()
+      end,
+      desc = 'Step Over',
+    },
+    {
+      '<leader>dp',
+      function()
+        require('dap').pause()
+      end,
+      desc = 'Pause',
+    },
+    {
+      '<leader>dr',
+      function()
+        require('dap').repl.toggle()
+      end,
+      desc = 'Toggle REPL',
+    },
+    {
+      '<leader>ds',
+      function()
+        require('dap').session()
+      end,
+      desc = 'Session',
+    },
+    {
+      '<leader>dt',
+      function()
+        require('dap').terminate()
+      end,
+      desc = 'Terminate',
+    },
+    {
+      '<leader>dw',
+      function()
+        require('dap.ui.widgets').hover()
+      end,
+      desc = 'Widgets',
+    },
+    {
+      '<leader>du',
+      function()
+        require('dapui').toggle {}
+      end,
+      desc = 'Dap UI',
+    },
+    {
+      '<leader>de',
+      function()
+        require('dapui').eval()
+      end,
+      desc = 'Eval',
+      mode = { 'n', 'v' },
+    },
+  },
+
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
@@ -77,56 +224,23 @@ return {
       dapui.close()
     end
 
-    -- Keymaps for debugging
-    vim.keymap.set('n', '<F5>', function()
-      dap.continue()
-    end)
-    vim.keymap.set('n', '<F10>', function()
-      dap.step_over()
-    end)
-    vim.keymap.set('n', '<F11>', function()
-      dap.step_into()
-    end)
-    vim.keymap.set('n', '<F12>', function()
-      dap.step_out()
-    end)
-    vim.keymap.set('n', '<Leader>b', function()
-      dap.toggle_breakpoint()
-    end)
-    vim.keymap.set('n', '<Leader>B', function()
-      dap.set_breakpoint()
-    end)
-    vim.keymap.set('n', '<Leader>lp', function()
-      dap.set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
-    end)
-    vim.keymap.set('n', '<Leader>dr', function()
-      dap.repl.open()
-    end)
-    vim.keymap.set('n', '<Leader>dl', function()
-      dap.run_last()
-    end)
+    -- load mason-nvim-dap here, after all adapters have been setup
+    if LazyVim.has 'mason-nvim-dap.nvim' then
+      require('mason-nvim-dap').setup(LazyVim.opts 'mason-nvim-dap.nvim')
+    end
 
-    -- Setup for nvim-dap-virtual-text
-    require('nvim-dap-virtual-text').setup()
+    vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
 
-    -- Setup for telescope-dap
-    require('telescope').load_extension 'dap'
+    for name, sign in pairs(LazyVim.config.icons.dap) do
+      sign = type(sign) == 'table' and sign or { sign }
+      vim.fn.sign_define('Dap' .. name, { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] })
+    end
 
-    -- Additional Telescope keymaps for DAP
-    vim.keymap.set('n', '<Leader>dc', function()
-      require('telescope').extensions.dap.commands {}
-    end)
-    vim.keymap.set('n', '<Leader>dC', function()
-      require('telescope').extensions.dap.configurations {}
-    end)
-    vim.keymap.set('n', '<Leader>db', function()
-      require('telescope').extensions.dap.list_breakpoints {}
-    end)
-    vim.keymap.set('n', '<Leader>dv', function()
-      require('telescope').extensions.dap.variables {}
-    end)
-    vim.keymap.set('n', '<Leader>df', function()
-      require('telescope').extensions.dap.frames {}
-    end)
+    -- setup dap config by VsCode launch.json file
+    local vscode = require 'dap.ext.vscode'
+    local json = require 'plenary.json'
+    vscode.json_decode = function(str)
+      return vim.json.decode(json.json_strip_comments(str))
+    end
   end,
 }
